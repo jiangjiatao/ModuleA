66cd8d41d05e6f8116a6b1dac7d87d12eb1e1d20
xiaotao <937840988@qq.com>
2019-11-17 23:29:54 +0800

    Initial Commit

---
+++ ModuleA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModuleA
+//
+//  Created by RJ on 2019/11/17.
+//  Copyright © 2019年 RJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModuleA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleA
+//
+//  Created by RJ on 2019/11/17.
+//  Copyright © 2019年 RJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
